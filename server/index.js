cd162652acbfb6f19fbe17ac96c5c9026f48804f
Pranshu Sama <23je0733@iitism.ac.in>
2026-07-16 17:38:01 +0530

    handling url's

--- server/index.js
+++ server/index.js
@@ -1,15 +1,20 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const myserver = http.createServer((req, res) => {
+    if (req.url === '/favicon.ico') return res.end();
     const log = `${Date.now()}: ${req.url}, New request received\n`;
+    const myUrl = url.parse(req.url,true);
+    console.log(myUrl);
     fs.appendFile("log.txt",log, (err,data) => {
-        switch(req.url) {
+        switch(myUrl.pathname) {
             case '/' :
                 res.end("Homepage");
                 break;
             case '/about':
-                res.end("Hey Im Pranshu")
+                const username = myUrl.query.myname;
+                res.end(`Heyy, ${username}`);
                 break;
             default :
                 res.end("404 not found")
