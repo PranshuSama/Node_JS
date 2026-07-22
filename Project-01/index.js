7cd8773c6845222b85acee1284d05871f61a71c8
Pranshu Sama <23je0733@iitism.ac.in>
2026-07-23 00:28:56 +0530

    added patch, delete route using middleware

--- Project-01/index.js
+++ Project-01/index.js
@@ -1,8 +1,13 @@
 const express = require("express")
 const users = require("./MOCK_DATA.json");
+const fs = require("fs");
 
 const app = express();
 const PORT = 8000;
+
+// Middleware - Plugins
+app.use(express.json());
+app.use(express.urlencoded({extended : false}));
 
 app.get("/users" , (req,res) => {
     const html = `
@@ -27,19 +32,41 @@ app
         res.json(user);
     })
     .patch((req,res) => {
-        // TODO : edit user with id
-        return res.json({status : "Pending"});
+        const Id = Number(req.params.id);
+        const body = req.body;
+        const user = users.find(user => user.id === Id);
+
+        if (!user) {
+            return res.status(404).json({status: "User not found"});
+        }
+
+        Object.assign(user, body);
+        fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+            return res.json({status: "success"});
+        });
     })
     .delete((req,res) => {
-        // TODO : delete user with id
-        return res.json({status : "Pending"});
+        const Id = Number(req.params.id);
+        const userIndex = users.findIndex(user => user.id === Id);
+
+        if (userIndex === -1) {
+            return res.status(404).json({status: "User not found"});
+        }
+
+        users.splice(userIndex, 1);
+        fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+            return res.json({status: "success"});
+        });
     }
 );
 
 app.post("/api/users", (req,res) => {
     // TODO : create a new user with id
-    return res.json({status : "Pending"});
-
+    const body = req.body;
+    users.push({id: users.length + 1 , ...body});
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err,data) => {
+        return res.json({status : "sucess", id : users.length});
+    });
 });
 
 app.listen(PORT, () => console.log(`Server is started at PORT: ${PORT}`));
