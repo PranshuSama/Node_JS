42f1b539ecdd3fde848de6e3adec563e6f8c17f5
Pranshu Sama <23je0733@iitism.ac.in>
2026-07-28 15:16:27 +0530

    feat: initialize project with express, mongoose, ejs templates, and user model

---
+++ Youtube-Blog/models/user.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require("mongoose");
+const { creatHmac } = requie("crypto");
+
+const userSchema = new Schema({
+    fullName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    salt: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    profileImageURL: {
+        type: String,
+        default: "/Users/pranshusama/Downloads/Node-JS/Youtube-Blog/public/images/default.png"
+
+    },
+    role: {
+        type: String,
+        enum: ["USER", "ADMIN"],
+        default: "USER",
+    }
+}, { timestamps: true });
+
+const User = model("user", userSchema);
+
+module.exports = {
+    User,
+}
